0c250e48d86b448b0a7ae5f786883d1e8e6d11b8
Umar Abdullahi <u.abdullahi001@gmail.com>
2019-04-14 11:17:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+$(function(){
+    //WOW plugin init
+    new WOW().init();
+
+    //parallax effect for banner
+    (function() {
+        var posY;
+        var image = document.getElementById('parallax');;
+        function paralax() {
+            posY = window.pageYOffset;
+            image.style.top = posY * 0.9 + 'px';
+        }
+        window.addEventListener('scroll', paralax);
+    })();
+});
